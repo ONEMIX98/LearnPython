14370dc1cb27d01b382a73f0342de961e3ad4de8
kevin <531784086@qq.com>
2017-08-02 17:50:04 +0800

    Add decorator.py

---
+++ decorator.py
@@ -0,0 +1,22 @@
+import functools
+
+def log(*args):
+    text = args[0] if isinstance(args[0],str) else ''
+    def decorator(func):
+        @functools.wraps(func) #保持装饰后的函数的函数名不变
+        def wrapper(*args, **kw):
+            print(f"begin {text} {func.__name__}():")
+            result = func(*args, **kw)
+            print(f"end {text} {func.__name__}()")
+            return result
+        return wrapper
+    return decorator if isinstance(args[0],str) else decorator(args[0])
+
+# 相当于 now = log('excute')(now), 先执行log('excute'),返回
+# decorator 函数。 再执行decorator(now), 最终返回的是wrapper函数
+@log('excute')
+def now():
+    print("2017-8-2")
+
+now()
+print("now.__name__: ", now.__name__)
